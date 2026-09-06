53b93807c0f319625ba736b6395de6cb15ec23ad
1FantaMan <turtleman040@gmail.com>
2026-09-06 14:52:32 +0300

    Fix indentation in gearSlots.lua
    Signed-off-by: 1FantaMan <turtleman040@gmail.com>

--- lua/beatrun/sh/gearSlots.lua
+++ lua/beatrun/sh/gearSlots.lua
@@ -1,22 +1,22 @@
 local mod = {}
 mod.SLOTS = {
-  right = { active = false },
-  left = {
-    active = true,
-    defaultKey = KEY_E
-  },
-  back = {
-    active = true,
-    defaultKey = KEY_X
-  },
-  leg = {
-    active = true,
-    defaultKey = KEY_G
-  }
+	right = { active = false },
+	left = {
+		active = true,
+		defaultKey = KEY_E
+	},
+	back = {
+		active = true,
+		defaultKey = KEY_X
+	},
+	leg = {
+		active = true,
+		defaultKey = KEY_G
+	}
 }
 
 function mod.IsValidSlot(slot)
-  return mod.SLOTS[slot] ~= nil
+	return mod.SLOTS[slot] ~= nil
 end
 
 return mod
